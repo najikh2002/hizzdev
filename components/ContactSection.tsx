d35a2f14ea72a0b24e311f1b93564d879d5a05f9
“najikh2002” <hizbullahnajihan@gmail.com>
2023-12-10 15:17:33 +0700

    feat: fixing contact section

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -3,22 +3,44 @@
 import { TitleSection } from "@/components";
 import emailjs from "@emailjs/browser";
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const ContactSection = () => {
     const messageRef = useRef<HTMLTextAreaElement>(null);
     const form = useRef<HTMLFormElement>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+    
+        // Perform form validation
+        const formData = new FormData(e.currentTarget);
+        const name = formData.get("name") as string;
+        const email = formData.get("email") as string;
+        const message = formData.get("message") as string;
+    
+        if (!name || !email || !message) {
+            alert("Nama, email, pesan jangan kosong!");
+            return;
+        }
+    
+        setLoading(true);
+    
         emailjs.sendForm('service_q9y9cuj', 'template_b39oalg', e.currentTarget, 'el_XVuhJlJR-1l92S')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-    }
+            .then((result) => {
+                console.log(result.text);
+                alert("Pesan berhasil terkirim!");
+            })
+            .catch((error) => {
+                console.log(error.text);
+                alert("Pesan gagal terkirim!");
+
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+    
 
     return ( 
         <section className="about-bg" id="contact">
@@ -37,11 +59,15 @@ const ContactSection = () => {
                 <form ref={form} onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)} className="flex flex-col gap-[56px] justify-center items-center">
 
                     <input type="text" placeholder="ENTER YOUR NAME" className="input h-[48px]" style={{ background: "none" }} name="name" />
-                    <input type="text" placeholder="ENTER YOUR EMAIL" className="input h-[48px]" style={{ background: "none" }} name="email" />
+                    <input type="email" placeholder="ENTER YOUR EMAIL" className="input h-[48px]" style={{ background: "none" }} name="email" />
                     <textarea placeholder="YOUR MESSAGE" className="input h-[184px] text-start justify-start items-center" style={{ background: "none" }} name="message" ref={messageRef} />
 
-                    <button type="submit" className="w-[177px] h-[40px] border-x-[4px] border-black uppercase text-[16px] font-bold tracking-[1.6px] lg:hover:w-[197px] transition-all duration-300 lg:cursor-pointer">
-                        submit
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="w-[177px] h-[40px] border-x-[4px] border-black uppercase text-[16px] font-bold tracking-[1.6px] lg:hover:w-[197px] transition-all duration-300 lg:cursor-pointer"
+                    >
+                        {loading ? "Submitting..." : "Submit"}
                     </button>
                 </form>
             </div>
